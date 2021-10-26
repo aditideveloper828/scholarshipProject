3b41b109b8fb0ae3592eb323a939a514c635dbc5
aditi <aditisharma828@gmail.com>
2021-10-26 21:48:53 +1300

    timestamp added

--- qrScanApp/routes.py
+++ qrScanApp/routes.py
@@ -7,7 +7,6 @@ from qrScanApp.models import User, Log, Code
 from werkzeug.urls import url_parse
 
 
-
 @application.route('/', methods=['GET', 'POST'])
 @application.route('/index', methods=['GET', 'POST'])
 @login_required
@@ -16,32 +15,20 @@ def index():
 
 @application.route('/trial', methods=['GET', 'POST'])
 def trial():
-    information = request.json("")
-    if information == "http":
-        return "1"
+    information = request.get_json()
+    code = Code.query.filter_by(code=information["info"]).first()
+    if code is not None:
+        log = Log(identity=current_user.identity)
+        storage.session.add(log)
+        storage.session.commit()
+        response = jsonify(success=True)
+        return response
     else:
-        return "2"
-    #if request.method == 'POST':
-    #    information = request.json()
-    #    log = Log(student=17529)
-    #    storage.session.add(log)
-    #    storage.session.commit()
-    #    return redirect(url_for('index'))
-    #else:
-    #    return redirect(url_for('register'))    
-
+        return "Error"
     
 
 @application.route('/login', methods=['GET', 'POST'])
 def login():
-    #if request.method == 'POST':
-    #    info = ""
-    #    info = request.json()
-    #    print(info)
-        #check = Code.query.filter_by(code=info).first()
-    #    log = Log(student=17529)
-    #    storage.session.add(log)
-    #    storage.session.commit()
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = LoginForm()
